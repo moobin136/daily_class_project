76ade12eab4d1176a24a816b9dbfa194128fdf92
moobin <moobinislam3@gmail.com>
2025-08-12 10:33:00 +0600

    gext

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'app.dart';
-
-void main(){
-  runApp(Myapp());
+void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  RxInt counte = 0.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter(),
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(title: Text('AppBar')),
+      body: Center(
+        child: Obx(
+          () => Text(
+            '$counte',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void counter() {
+    counte++;
+    print(counte);
+  }
+}
