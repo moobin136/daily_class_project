e81b5d3fe0bbabff0b32b382205c25af90ad7689
moobin <moobinislam3@gmail.com>
2025-08-12 12:31:54 +0600

    gextdvbcv

---
+++ lib/counterController.dart
@@ -0,0 +1,10 @@
+// import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+//
+// class CounterControlle extends GetxController {
+//   int counter = 0;
+//
+//   void increment() {
+//     counter++;
+//     update();
+//   }
+// }
