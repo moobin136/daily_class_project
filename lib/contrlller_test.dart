b0a93fc2b7f533e65fff543a1dbb193742c58b42
moobin <moobinislam3@gmail.com>
2025-09-06 09:55:05 +0600

    delet

--- lib/contrlller_test.dart
+++
@@ -1,7 +0,0 @@
-// // gender_controller.dart
-// import 'package:get/get.dart';
-//
-// class GenderController extends GetxController {
-//   final List<String> genders = ["Everyone", "Man", "Woman", "Copy"];
-//   var selectedGender = "Everyone".obs;
-// }
